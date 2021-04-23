8c813e9186e5f29c22ab507942e8ebf330e1339e
Chuzzy <thechuzzyguy@gmail.com>
2021-04-23 23:54:58 +0100

    Add exit menu item handler

--- src/main/java/application/PrimaryController.java
+++ src/main/java/application/PrimaryController.java
@@ -1,9 +1,17 @@
 package application;
 
 import java.io.IOException;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
+
+    @FXML
+    private void exitApp() {
+        System.out.println("Exiting app...");
+        Platform.exit();
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
