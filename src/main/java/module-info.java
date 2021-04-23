5513df4bad48be5965d3622fba428716492a5839
Chuzzy <thechuzzyguy@gmail.com>
2021-04-23 16:07:29 +0100

    Add transitive requirements to module-info javafx.base and javafx.graphics are now listed as "requires transitive".

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module application {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.base;
+    requires transitive javafx.graphics;
 
     opens application to javafx.fxml;
     exports application;
