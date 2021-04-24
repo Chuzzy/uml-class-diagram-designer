e9a03a5aa233df62334e8fe95edac39fdb1560e9
Chuzzy <thechuzzyguy@gmail.com>
2021-04-24 13:51:57 +0100

    Add a button to AttributeList control Uses FXML to load the structure.

--- src/main/java/application/AttributeList.java
+++ src/main/java/application/AttributeList.java
@@ -1,7 +1,22 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.VBox;
 
 public class AttributeList extends VBox {
     
+
+    public AttributeList() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("AttributeList.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
