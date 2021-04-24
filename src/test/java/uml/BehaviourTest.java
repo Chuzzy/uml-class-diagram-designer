37271015b5bc908402bc075ae6ad6ef2a39ac4d0
Chuzzy <thechuzzyguy@gmail.com>
2021-04-24 20:12:47 +0100

    Rename BehaviourTests to BehaviourTest

--- src/test/java/uml/BehaviourTest.java
+++ src/test/java/uml/BehaviourTest.java
@@ -4,16 +4,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class BehaviourTests {
+public class BehaviourTest {
     
     @Test
-    void toStringBehaviourWithNoArguments() {
+    public void toStringBehaviourWithNoArguments() {
         Behaviour noArgBehaviour = new Behaviour(Access.PUBLIC, "void", "myBehaviour");
         assertEquals("Behaviour [public void myBehaviour()]", noArgBehaviour.toString());
     }
 
     @Test
-    void toStringBehaviourWithOneArgument() {
+    public void toStringBehaviourWithOneArgument() {
         Behaviour oneArgBehaviour = new Behaviour(Access.PRIVATE, "int", "log", new Argument("int", "x"));
         assertEquals("Behaviour [private int log(int x)]", oneArgBehaviour.toString());
     }
