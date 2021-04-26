3e42cfe9c805444be6ea78bc329b5bc3f2f572a5
Chuzzy <thechuzzyguy@gmail.com>
2021-04-26 14:10:45 +0100

    Add some style to the App

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,12 +1,13 @@
 package application;
 
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 /**
  * The application bootloader.
@@ -24,6 +25,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        setStyle("style");
         stage.setScene(scene);
         stage.show();
     }
@@ -48,6 +50,15 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    static void setStyle(String css) {
+        scene.getStylesheets().clear();
+        scene.getStylesheets().add(loadStylesheet(css).toExternalForm());
+    }
+
+    private static URL loadStylesheet(String css) {
+        return App.class.getResource(css + ".css");
     }
 
     /**
